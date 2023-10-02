10e3d63dd86a9c72d700497fc11429876b6950fd
zoe park <hellopark821@gmail.com>
2023-10-03 01:29:38 +0900

    feat: adding 'playground.dart'

---
+++ lib/screen/playground.dart
@@ -0,0 +1,143 @@
+/*
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text(
+          '캥거루',
+          style: TextStyle(
+              color: Color(0xFFF79E89),
+              fontWeight: FontWeight.bold,
+              fontSize: 24),
+        ),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
+            height: 150,
+            width: 820,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 10.0,
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+                  child: Text(
+                    '피그마',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20),
+                  ),
+                ),
+                SizedBox(
+                  height: 12.0,
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+                  child: Text(
+                    'abcd@gmail.com',
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Padding(
+                    padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+                    child: Text('12341234',
+                        style: TextStyle(color: Colors.white, fontSize: 18))),
+                SizedBox(
+                  height: 15.0,
+                ),
+                Padding(
+                    padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+                    child: Text('-',
+                        style: TextStyle(color: Colors.white, fontSize: 16))),
+              ],
+            ),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              color: Color(0xFFF76C6A),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
+            height: 150,
+            width: 820,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 10.0,
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+                  child: Text(
+                    '깃허브',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20),
+                  ),
+                ),
+                SizedBox(
+                  height: 12.0,
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+                  child: Text(
+                    'abcd@gmail.com',
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Container(
+                    padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+                    child: Text('12341234',
+                        style: TextStyle(color: Colors.white, fontSize: 18))),
+                SizedBox(
+                  height: 15.0,
+                ),
+                Container(
+                    padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+                    child: Text('구글 연동',
+                        style: TextStyle(color: Colors.white, fontSize: 16))),
+              ],
+            ),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              color: Color(0xFFF79E89),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+        backgroundColor: Color(0xFFF76C6A),
+      ),
+    );
+  }
+}
+*/
