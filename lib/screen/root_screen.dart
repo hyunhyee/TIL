30ad26fd8a59eddd82fd44e6c9feadfa7b163800
zoe park <hellopark821@gmail.com>
2023-10-18 21:13:24 +0900

    feat: Add tabcontroller

--- lib/screen/root_screen.dart
+++ lib/screen/root_screen.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class RootScreen extends StatelessWidget {
+class RootScreen extends StatefulWidget {
   const RootScreen({Key? key}) : super(key: key);
+
+  @override
+  State<RootScreen> createState() => _RootScreenState();
+}
+
+class _RootScreenState extends State<RootScreen> with TickerProviderStateMixin {
+  TabController? controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = TabController(length: 2, vsync: this);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: TabBarView(
+        controller: controller,
         children: renderChildren(),
       ),
       bottomNavigationBar: renderBottomNavigation(),
