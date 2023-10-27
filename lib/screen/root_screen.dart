68b31db68a27a208b883ba0bf0513b45f316f948
zoe park <hellopark821@gmail.com>
2023-10-28 04:34:36 +0900

    feat: addListener() 함수에 콜백 함수 setState() 등록

--- lib/screen/root_screen.dart
+++ lib/screen/root_screen.dart
@@ -16,13 +16,15 @@ class _RootScreenState extends State<RootScreen> with TickerProviderStateMixin {
   void initState() {
     super.initState();
     controller = TabController(length: 2, vsync: this); //상태 유지
-
     controller!.addListener(tabListener);
   }
 
   tabListener() {
     setState(() {});
   }
+  //addListener() 함수는 controller 의 속성이 변할 때마다 특정 함수를 실행할 수 있도록 콜백 함수 등록 가능
+  //이 콜백 함수에 setState()를 실행하여 controller 속성이 변경될 때마다 build()를 재실행
+  //RootScreen 위젯이 생성될 때 단 한번만 리스너가 등록되면 되니까 initState()에서 실행
 
   @override
   void dispose() {
@@ -63,15 +65,22 @@ class _RootScreenState extends State<RootScreen> with TickerProviderStateMixin {
   }
 
   BottomNavigationBar renderBottomNavigation() {
-    return BottomNavigationBar(items: [
-      BottomNavigationBarItem(
-        icon: Icon(Icons.edgesensor_high_outlined),
-        label: '주사위',
-      ),
-      BottomNavigationBarItem(
-        icon: Icon(Icons.settings),
-        label: '설정',
-      ),
-    ]);
+    return BottomNavigationBar(
+        currentIndex: controller!.index,
+        onTap: (int index) {
+          setState(() {
+            controller!.animateTo(index);
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.edgesensor_high_outlined),
+            label: '주사위',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: '설정',
+          ),
+        ]);
   }
 }
