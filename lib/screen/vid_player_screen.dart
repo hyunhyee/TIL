8103e04d52ff55b156214192401d363fa04b1084
ARi <hellopark821@gmail.com>
2024-01-10 23:51:09 +0900

    feat : Change stateless widget to stateful widget

--- lib/screen/vid_player_screen.dart
+++ lib/screen/vid_player_screen.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class VidPlayScreen extends StatelessWidget {
+class VidPlayScreen extends StatefulWidget {
   const VidPlayScreen({Key? key}) : super(key: key);
 
+  @override
+  State<VidPlayScreen> createState() => _VidPlayScreenState();
+}
+
+class _VidPlayScreenState extends State<VidPlayScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
